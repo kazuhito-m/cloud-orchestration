988ca9369bec4590511b564ef9f0194c7d52e396
kazuhito_m <kazuhito.sumpic@gmail.com>
2019-08-27 01:24:59 +0900

    refactor: manufestを一つに統合したので、元と成った周辺ファイルは削除。

--- gcp/gke/growi/create.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in namespace mongo elasticsearch growi-app
-do
-  kubectl create -f ./${i}.yaml
-done
--- gcp/gke/growi/delete.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in growi-app elasticsearch mongo namespace
-do
-  kubectl delete -f ./${i}.yaml
-done
